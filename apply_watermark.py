0c350e9a22bdcbeee37aa411ad092f6ab639c860
Colin Pate <pate.colin@gmail.com>
2024-07-21 19:09:30 -0700

    FIxed watermark colors

--- apply_watermark.py
+++ apply_watermark.py
@@ -4,6 +4,7 @@ import numpy as np
 class ApplyWatermark:
     def __init__(self, watermark_path, watermark_position="lr", weight=1, h_size=0, offset_x=0, offset_y=0):
         watermark_in = cv2.imread(watermark_path, cv2.IMREAD_UNCHANGED)
+        watermark_in = cv2.cvtColor(watermark_in, cv2.COLOR_RGBA2BGRA)
         if h_size == 0:
             watermark = watermark_in
         else:
@@ -58,7 +59,7 @@ if __name__ == "__main__":
     watermarker = ApplyWatermark(
         watermark_path=watermark_path,
         weight=1,
-        h_scale=0.25, 
+        h_size=950, 
         offset_x=100, 
         offset_y=90
         )
