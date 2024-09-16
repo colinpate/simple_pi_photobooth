48b3788a23631cde88864ef7f5ea3621380c8aed
Colin Pate <pate.colin@gmail.com>
2024-09-16 14:56:32 -0700

    Updated overlay manager

--- booth/overlay_manager.py
+++ booth/overlay_manager.py
@@ -2,35 +2,64 @@ from collections import OrderedDict
 import numpy as np
 import time
 
+class Layer:
+    def __init__(self, image):
+        self.raw_image = image
+        self._active = False
+        alpha_1chan = np.array(image[:,:,3], dtype=np.float32) / 255
+        alpha = np.stack([alpha_1chan]*3, axis=-1)
+        self.alpha_inv = np.ones(alpha.shape, dtype=np.float32) - alpha
+        self.rgb = image[:,:,:3] * alpha
+        self.alpha = image[:,:,3]
+        
+    def composite(self, image):
+        image[:, :, :3] = (image[:,:,:3] * self.alpha_inv) + self.rgb
+        # Just add the alphas
+        image[:,:,3] = np.clip(image[:,:,3] + self.alpha, a_min=0, a_max=255) 
+        
+    def is_active(self):
+        return self._active
+        
+    def activate(self)
+        self._active = True
+        
+    def deactivate(self):
+        self._active = False
+        
+
 class OverlayManager:
     def __init__(self, display_width, display_height):
         self.layers = OrderedDict()
-        self.layers["main"] = None
-        self.main_layer_exclusive = False
+        self.main_image = None
+        self.main_image_exclusive = False
         self.layers_changed = True
         self.display_width = display_width
         self.display_height = display_height
         
-    def set_layer(self, layer, name="main", exclusive=False):
+    def set_layer(self, image, name):
+        self.layers[name] = Layer(image)
+        
+    def activate_layer(self, name):
         self.layers_changed = True
-        if exclusive:
-            if not (name == "main"):
-                raise ValueError("Only the main layer can be exclusive")
-            self.main_layer_exclusive = True
-            self.layers["main"] = layer
-        else:
-            if name == "main":
-                self.main_layer_exclusive = False
-            self.layers[name] = layer
+        self.layers[name].activate()
+        
+    def deactivate_layer(self, name):
+        self.layers_changed = True
+        self.layers[name].deactivate()
+        
+    def set_main_image(self, image, exclusive):
+        self.layers_changed = True
+        self.main_image = image
+        self.main_image_exclusive = exclusive
         
     def update_overlay(self):
         if self.layers_changed:
             start_time = time.time()
             self.layers_changed = False
-            if self.main_layer_exclusive:
-                overlay = self.layers["main"]
+            if self.main_image_exclusive:
+                overlay = self.main_image
             else:
-                if not (self.layers["main"] is not None):
+                if self.main_image is None:
                     is_empty = True
                     overlay = np.zeros(
                             (
@@ -42,18 +71,12 @@ class OverlayManager:
                         )
                 else:
                     is_empty = False
-                    overlay = self.layers["main"]
+                    overlay = self.main_image
                 
                 for name, layer in self.layers.items():
-                    if (layer is not None) and (name != "main"):
+                    if layer.is_active():
                         is_empty = False
-                        layer_alpha_1chan = np.array(layer[:,:,3], dtype=np.float32) / 255
-                        layer_alpha = np.stack([layer_alpha_1chan]*3, axis=-1)
-                        layer_alpha_inv = np.ones(layer_alpha.shape, dtype=np.float32) - layer_alpha
-                        layer_rgb = layer[:,:,:3] * layer_alpha
-                        overlay[:, :, :3] = (overlay[:,:,:3] * layer_alpha_inv) + layer_rgb
-                        # Just add the alphas
-                        overlay[:,:,3] = np.clip(overlay[:,:,3] + layer[:,:,3], a_min=0, a_max=255) 
+                        layer.composite(overlay)
                         
                 if is_empty:
                     overlay = None
