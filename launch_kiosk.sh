c4d9241989f41c366fffb8840574286a9f3ac89e
Colin Pate <pate.colin@gmail.com>
2024-10-10 19:23:22 -0700

    Made service for s3 uploader

--- launch_kiosk.sh
+++ launch_kiosk.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#source /home/colin/simple_pi_photobooth/booth_venv/bin/activate
 export PYTHONPATH=$PYTHONPATH:/home/colin/simple_pi_photobooth
 python3 /home/colin/simple_pi_photobooth/print_kiosk/kiosk_gui.py 2> /home/colin/fail_log_print.txt
