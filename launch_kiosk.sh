c11347a0d0002aad577f3bb76d195d9c7fe7bdfc
Colin Pate <pate.colin@gmail.com>
2025-09-16 21:15:50 -0700

    removed error catching from launch_kiosk.sh

--- launch_kiosk.sh
+++ launch_kiosk.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:/home/colin/simple_pi_photobooth
-python3 /home/colin/simple_pi_photobooth/print_kiosk/kiosk_gui.py 2> /home/colin/fail_log_print.txt
+python3 /home/colin/simple_pi_photobooth/print_kiosk/kiosk_gui.py
